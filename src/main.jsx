9190c39af0e1150d5f5dbda599a59a7311b37132
Barathkumar-tech <baratharjunx@gmail.com>
2026-08-17 16:35:16 +0530

    Add Google Analytics 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import './index.css'
 import App from './App.jsx'
+
+ReactGA.initialize('G-MVWP5TH3R9')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
